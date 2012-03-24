ece4f6e8d4cca3d03fc73fde70043c88ef495133
Magnus Larsson <maguaking@gmail.com>
2012-03-24 01:14:51 +0100

    This version works with curl, the old one works with unit tests???

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,14 +10,12 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
-  
-  
-  
-  
+
   def jsonNew = Action { request =>
-  	val absenceJson = request.body.toString()
-  	val absence = Json.parse(absenceJson).as[Absence]
+	val absence = request.body.asJson.map(_.as[Absence]).getOrElse(
+	  throw new RuntimeException("could not create user")
+	)
+
 	val id = Absence.create(absence)
     Ok("""{"rc":0,"message":"Ok"}""").as("application/json")
   }
